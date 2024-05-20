6c6a8c8f9614d278804143f372fc54874f666d3c
Konrad Początek <konrad.poczatek@leovegas.com>
2024-05-20 20:32:54 +0200

    Init backend module^

---
+++ backend/src/test/java/aiversum/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package aiversum.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
