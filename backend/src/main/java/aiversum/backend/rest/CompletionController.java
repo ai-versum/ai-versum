5811900bd60edee12ee87bf18f60d3d4933d1ee9
Konrad Początek <konrad.poczatek@leovegas.com>
2024-05-21 20:48:41 +0200

    Configure ollama base url in application.properties^

--- backend/src/main/java/aiversum/backend/rest/CompletionController.java
+++ backend/src/main/java/aiversum/backend/rest/CompletionController.java
@@ -1,6 +1,7 @@
 package aiversum.backend.rest;
 
 import aiversum.backend.rest.dto.CompletionCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,10 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import static java.lang.StringTemplate.STR;
+
 @RestController
 @RequestMapping("/completion")
 public class CompletionController {
     private final WebClient webClient;
+
+    @Value("${ai.versum.ollama.baseUrl}")
+    private String ollamaBaseUrl;
 
     public CompletionController(WebClient webClient) {
         this.webClient = webClient;
@@ -21,7 +27,7 @@ public class CompletionController {
     @PostMapping(value = "/generate/{provider}", produces = "application/json")
     public Mono<String> generate(@PathVariable String provider, @RequestBody CompletionCommand completionCommand) {
         if (provider.equals("ollama")) {
-            return webClient.post().uri("http://localhost:11434/api/generate")
+            return webClient.post().uri(STR."\{ollamaBaseUrl}/api/generate")
                     .body(Mono.just(completionCommand), CompletionCommand.class)
                     .retrieve()
                     .bodyToMono(String.class);
--- backend/src/main/java/aiversum/backend/rest/ModelController.java
+++ backend/src/main/java/aiversum/backend/rest/ModelController.java
@@ -1,5 +1,6 @@
 package aiversum.backend.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,16 @@ import reactor.core.publisher.Mono;
 public class ModelController {
     private final WebClient webClient;
 
+    @Value("${ai.versum.ollama.baseUrl}")
+    private String ollamaBaseUrl;
+
     public ModelController(WebClient webClient) {
         this.webClient = webClient;
     }
 
     @GetMapping(value = "/ollama", produces = "application/json")
     public Mono<String> listModels() {
-        return webClient.get().uri("http://localhost:11434/api/tags")
+        return webClient.get().uri(STR."\{ollamaBaseUrl}/api/tags")
                 .retrieve()
                 .bodyToMono(String.class);
     }
