b4a5f4844210be4bb3ed9ddc193e70721a3609b0
Konrad Początek <konrad.poczatek@leovegas.com>
2024-05-27 09:11:01 +0200

    Extract chat content to separate component

---
+++ frontend/src/lib/components/ChatContent.svelte
@@ -0,0 +1,24 @@
+<script>
+import { marked } from 'marked';
+
+export let responseMessage = '';
+export let isError = false;
+</script>
+
+<p class:error={isError}>{@html marked(responseMessage)}</p>
+
+<style>
+    p {
+        color: var(--text-color);
+        background-color: #e9e9e9;
+        padding: 10px;
+        border-radius: 4px;
+        border-left: 5px solid var(--primary-color);
+    }
+
+    p.error {
+        background-color: var(--error-color);
+        color: #fff;
+        border-color: #f00; /* Make it stand out more */
+    }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-	import { marked } from 'marked';
 	import MessageInput from '$lib/components/MessageInput.svelte';
 	import ModelSelect from '$lib/components/ModelSelect.svelte';
+	import ChatContent from '$lib/components/ChatContent.svelte';
 
 	let selectedModel;
 	let responseMessage = '';
@@ -11,25 +11,12 @@
 <ModelSelect on:modelChange={(e) => { selectedModel = e.detail; }} />
 <hr>
 
-<p class:error={isError}>{@html marked(responseMessage)}</p>
+<ChatContent
+	isError="{isError}"
+	responseMessage="{responseMessage}"
+/>
 
 <MessageInput
 	on:errorMessage={(e) => { responseMessage = e.detail; isError = true; }}
 	on:message={(e) => { responseMessage = e.detail; isError = false; }}
 	selectedModel="{selectedModel}" />
-
-<style>
-    p {
-        color: var(--text-color);
-        background-color: #e9e9e9;
-        padding: 10px;
-        border-radius: 4px;
-        border-left: 5px solid var(--primary-color);
-    }
-
-    p.error {
-        background-color: var(--error-color);
-        color: #fff;
-        border-color: #f00; /* Make it stand out more */
-    }
-</style>
