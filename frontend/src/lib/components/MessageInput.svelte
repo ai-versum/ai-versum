15c8343491df96e66358e0ca0d04d62a23112025
Konrad Początek <konrad.poczatek@leovegas.com>
2024-05-31 16:58:37 +0200

    Add smui

--- frontend/src/lib/components/MessageInput.svelte
+++ frontend/src/lib/components/MessageInput.svelte
@@ -1,4 +1,11 @@
 <script>
+	import { Input } from '@smui/textfield';
+	import Paper from '@smui/paper';
+	import Fab from '@smui/fab';
+	import { Icon } from '@smui/common';
+
+	let value = '';
+
 	import { fetchOllamaChat } from '$lib/api/ollamaAPI.js';
 	import { fetchOpenAIChat } from '$lib/api/openaiAPI.js';
 	import { chatStore } from '../stores/ChatStore.js';
@@ -63,27 +70,50 @@
 	}
 </script>
 
-<div class="flex relative w-full rounded-3xl px-1.5 border border-gray-200 bg-white
-dark:border-gray-850 dark:bg-gray-900 dark:text-gray-100">
-	<textarea
-		id="chat-textarea"
-		class=" dark:bg-gray-900 dark:text-gray-100 outline-none w-full py-3 px-3 pl-4'} rounded-xl resize-none h-[48px]"
-		placeholder={'Send a message'}
-		bind:value={question}
-		on:keydown={handleKeyPress}
-	/>
-
-	<div class="self-end mb-2 flex space-x-1 mr-1">
-			<button
-				class="{question !== ''
-										? 'bg-black hover:bg-gray-900 dark:bg-white dark:text-black dark:hover:bg-gray-100 '
-										: 'disabled bg-gray-100 dark:text-gray-900 dark:bg-gray-800 '} text-white transition rounded-full p-1.5 self-center"
-				type="submit"
-				disabled={question === ''}
-			>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-					<path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-				</svg>
-			</button>
-	</div>
+<div class="flex justify-center items-center">
+	<Paper class="solo-paper" elevation={6}>
+		<Icon class="material-icons">search</Icon>
+		<Input
+			bind:value={question}
+			on:keydown={handleKeyPress}
+			placeholder="Search"
+			class="solo-input"
+		/>
+	</Paper>
+	<Fab
+		on:click={sendQuestion}
+		disabled={value?.trim() === ''}
+		color="primary"
+		mini
+		class="solo-fab"
+	>
+		<Icon class="material-icons ">arrow_forward</Icon>
+	</Fab>
 </div>
+
+<style>
+    * :global(.solo-paper) {
+        display: flex;
+        align-items: center;
+        flex-grow: 1;
+        max-width: 600px;
+        margin: 0 12px;
+        padding: 0 12px;
+        height: 48px;
+    }
+    * :global(.solo-paper > *) {
+        display: inline-block;
+        margin: 0 12px;
+    }
+    * :global(.solo-input) {
+        flex-grow: 1;
+        color: var(--mdc-theme-on-surface, #000);
+    }
+    * :global(.solo-input::placeholder) {
+        color: var(--mdc-theme-on-surface, #000);
+        opacity: 0.6;
+    }
+    * :global(.solo-fab) {
+        flex-shrink: 0;
+    }
+</style>
