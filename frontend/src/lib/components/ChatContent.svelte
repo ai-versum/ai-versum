c8b409966abf7f60575c14273c00d64aaf8821b7
Averath <kochamame@gmail.com>
2024-06-05 13:50:37 +0200

    Code highlight (#7)

--- frontend/src/lib/components/ChatContent.svelte
+++ frontend/src/lib/components/ChatContent.svelte
@@ -1,18 +1,20 @@
 <script>
-	import { marked } from 'marked';
 	import { chatStore } from '../stores/ChatStore.js';
 	import Card from '@smui/card';
+	import AIMessage from '$lib/components/AIMessage.svelte';
 </script>
 
 <ul>
 	{#each $chatStore as message}
-		<div class="mb-3">
+		<div class="mb-3 mx-auto flex flex-1 gap-3 text-base juice:gap-4 juice:md:gap-6 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]">
 			{#if (message.type === 'USER')}
-				<Card class="bg-gray-200 w-1/2 right-0 ml-auto rounded-2xl" padded>{@html marked(message.text)}</Card>
+				<Card class="bg-gray-200 w-1/2 right-0 ml-auto rounded-2xl" padded>{message.text}</Card>
 			{:else if (message.type === 'AI')}
-				<Card padded>{@html marked(message.text)}</Card>
+				<Card class="max-w-full" padded>
+					<AIMessage message="{message.text}" />
+				</Card>
 			{:else}
-				<p class:error={message.isError}>{@html marked(message.text)}</p>
+				<p class:error={message.isError}>{message.text}</p>
 			{/if}
 		</div>
 	{/each}
