55214a3c2ce812e993cb6d4ccdb67c98889529d5
xdeejm97 <d.niesmialowski@gmail.com>
2024-10-27 16:09:59 +0100

    Updated config variables to model

--- backend/src/main/java/aiversum/backend/rest/dto/UserConfigDto.java
+++ backend/src/main/java/aiversum/backend/rest/dto/UserConfigDto.java
@@ -1,11 +1,8 @@
 package aiversum.backend.rest.dto;
 
 public record UserConfigDto(
-        String imageSize,
-        String imageStyle,
-        String apiKey,
-        String enabled,
-        String projectName,
-        String location
-) {
+        OllamaConfig ollamaConfig,
+        OpenaiConfig openaiConfig,
+        VertexaiConfig vertexaiConfig
+        ) {
 }
